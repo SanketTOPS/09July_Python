079a1e4dd5edca0f274978a2ecb3e80504510eac
SanketTops <sanket.tops@gmail.com>
2025-09-11 17:48:01 +0530

    OOPS

---
+++ Assignments/Module-3_OOP/single_inh.py
@@ -0,0 +1,18 @@
+class father:
+    bal:int
+    car:int
+    
+    def getdata(self):
+        self.bal=input("Enter bank balance details:")
+        self.car=input("Enter a car details:")
+
+class son(father):
+    def printdata(self):
+        print("Balance:",self.bal)
+        print("Car:",self.car)
+
+sn=son()
+sn.getdata()
+sn.printdata()
+        
+    
