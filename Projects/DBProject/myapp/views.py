20204bf41ddea9a444d9cf928cbfc39fd688ecaf
SanketTops <sanket.tops@gmail.com>
2025-11-04 17:34:03 +0530

    Full CRUD

--- Projects/DBProject/myapp/views.py
+++ Projects/DBProject/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import *
 
 # Create your views here.
@@ -8,6 +8,29 @@ def index(request):
         if form.is_valid():
             form.save()
             print("Record Inserted!")
+            return redirect('showdata')
         else:
             print(form.errors)
     return render(request,'index.html')
+
+def showdata(request):
+    data=Userdata.objects.all()
+    return render(request,'showdata.html',{'data':data})
+
+def updatedata(request,id):
+    stid=Userdata.objects.get(id=id)
+    if request.method=='POST':
+        form=UserForm(request.POST,instance=stid)
+        if form.is_valid():
+            form.save()
+            print("Record Updated!")
+            return redirect('showdata')
+        else:
+            print(form.errors)
+    return render(request,'updatedata.html',{'stid':stid})
+
+def deletedata(request,id):
+    stid=Userdata.objects.get(id=id)
+    Userdata.delete(stid)
+    return redirect('showdata')
+    
