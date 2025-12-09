cd658d84cacaf23feb950595f2e69410620b67c6
SanketTops <sanket.tops@gmail.com>
2025-12-09 17:04:11 +0530

    Error Pages

--- Projects/FinalProject/FinalProject/settings.py
+++ Projects/FinalProject/FinalProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-52m&tl722@+9gjohz6wj(r=w(uw2z7=5p5)=uv8z(&69cqj#y1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
